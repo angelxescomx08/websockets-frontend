1d16eafef4c6c9e33df598d4f41069d34c2eecf4
Jose Angel <ah@xdevelop.mx>
2024-09-28 20:01:10 -0600

    enviar mensajes

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ socket.onclose = (event) => {
   console.log(event);
   console.log('Disconnected from server');
 }
+
+function sendMessage(message) {
+  socket.send(message);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('form');
+  const input = document.querySelector('input');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const message = input.value;
+    sendMessage(message);
+    input.value = '';
+  });
+});
