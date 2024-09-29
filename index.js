f01934f7bfde6b40e87e34f176460907cfb4d903
Jose Angel <ah@xdevelop.mx>
2024-09-28 20:19:51 -0600

    mostrar mensajes

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-const socket = new WebSocket('ws://localhost:3000');
-
-socket.onopen = (event) => {
-  console.log(event);
-  console.log('Connected to server');
-}
-
-socket.onclose = (event) => {
-  console.log(event);
-  console.log('Disconnected from server');
-}
-
-function sendMessage(message) {
+function sendMessage(message, socket) {
   socket.send(message);
 }
 
+function renderMessage(message, messagesElement) {
+  const li = document.createElement('li');
+  li.textContent = message;
+  messagesElement.prepend(li);
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+
+  const socket = new WebSocket('ws://localhost:3000');
+
+  socket.onopen = (event) => {
+    console.log(event);
+    console.log('Connected to server');
+  }
+
+  socket.onclose = (event) => {
+    console.log(event);
+    console.log('Disconnected from server');
+  }
+
+
   const form = document.querySelector('form');
   const input = document.querySelector('input');
+  const messages = document.querySelector('#messages');
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const message = input.value;
-    sendMessage(message);
+    sendMessage(message, socket);
     input.value = '';
   });
+
+  socket.onmessage = (event) => {
+    console.log(event);
+    const { payload } = JSON.parse(event.data);
+    console.log(payload);
+    renderMessage(payload, messages);
+  }
 });
